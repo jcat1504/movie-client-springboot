92c20f123b98084ed672151ada83306a100c779b
Jess Y <jyin978@hotmail.com>
2023-08-02 16:17:06 -0400

    changes with cross origin endpoint.

--- movie-client-springboot/src/main/java/unicorndev/movies/ReviewController.java
+++ movie-client-springboot/src/main/java/unicorndev/movies/ReviewController.java
@@ -10,7 +10,8 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://movie-gold-v1.onrender.com")
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
     @Autowired
