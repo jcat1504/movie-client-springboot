f55e09ac79b51002b932763ab32217145d80b34c
Jess Y <jyin978@hotmail.com>
2023-08-08 15:54:35 -0400

    corsConfig class and changes to moviesApplication

--- movie-client-springboot/src/main/java/unicorndev/movies/MoviesApplication.java
+++ movie-client-springboot/src/main/java/unicorndev/movies/MoviesApplication.java
@@ -2,33 +2,23 @@ package unicorndev.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.context.annotation.Import;
 
 
 @SpringBootApplication
 @RestController
+@Import(CorsConfig.class) // Import your CorsConfig class
 public class MoviesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MoviesApplication.class, args);}
-		@GetMapping("/root")
-		public String apiRoot() {
-			return "Hello World!";
+		SpringApplication.run(MoviesApplication.class, args);
 	}
 
-	@Bean
-	public CorsFilter corsFilter() {
-	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	CorsConfiguration config = new CorsConfiguration();
-	config.addAllowedOrigin("https://movie-gold-v1.onrender.com");
-	config.addAllowedHeader("*");
-	config.addAllowedMethod("*");
-	source.registerCorsConfiguration("/api/v1/movies", config); // Adjust the path as needed
-	return new CorsFilter(source);
+	@GetMapping("/root")
+	public String apiRoot() {
+		return "Hello World!";
 	}
 }
