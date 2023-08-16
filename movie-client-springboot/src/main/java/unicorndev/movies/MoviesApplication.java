28759e2f83b051286568b1af5ac688979ea74564
Jess Y <jyin978@hotmail.com>
2023-08-16 12:56:31 -0400

    CorsFilter, MoviesApplication class updated

--- movie-client-springboot/src/main/java/unicorndev/movies/MoviesApplication.java
+++ movie-client-springboot/src/main/java/unicorndev/movies/MoviesApplication.java
@@ -2,10 +2,11 @@ package unicorndev.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 
 @SpringBootApplication
@@ -19,5 +20,15 @@ public class MoviesApplication {
 	@GetMapping("/root")
 	public String apiRoot() {
 		return "Hello World!";
+	}
+
+	@Bean
+	public WebMvcConfigurer configure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry reg) {
+				reg.addMapping("/*").allowedOrigins("https://movie-gold-v1.onrender.com");
+			}
+		};
 	}
 }
